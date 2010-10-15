820f7cba40b759693fe71c3a436ca10523f61b8f
Niels Thykier <niels@thykier.net>
2010-10-15 10:33:45 +0200

    Made Statement less abstract, extended its interface and provided an StatementImpl.

--- src/com/github/mithunder/statements/Statement.java
+++ src/com/github/mithunder/statements/Statement.java
@@ -1,6 +1,38 @@
 package com.github.mithunder.statements;
 
+import java.util.Collections;
+import java.util.List;
+
 public abstract class Statement {
 
-	public abstract Variable getAssign();
+	protected int stype;
+	// Can be null in some cases (e.g. abort statements)
+	protected Variable assign;
+	protected List<Annotation> annotations;
+
+	protected Statement(int stype, Variable assign, List<Annotation> annotations){
+		this.stype = stype;
+		this.assign = assign;
+		if(annotations != null) {
+			this.annotations = Collections.unmodifiableList(annotations);
+		} else {
+			this.annotations = Collections.emptyList();
+		}
+	}
+
+	public int getStatementType(){
+		return stype;
+	}
+
+	public Variable getAssign(){
+		return assign;
+	}
+
+	public List<Annotation> getAnnotations(){
+		return annotations;
+	}
+
+	public abstract List<? extends Statement> getChildren();
+
+	public abstract Value[] getValues();
 }
