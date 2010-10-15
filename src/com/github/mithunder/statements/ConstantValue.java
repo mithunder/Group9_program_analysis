1207ba2ee02da88a7d0b08e5347f88b7f34f9546
Niels Thykier <niels@thykier.net>
2010-10-15 10:46:24 +0200

    Implement TRUE and FALSE + hashCode/equals for ConstantValue.

--- src/com/github/mithunder/statements/ConstantValue.java
+++ src/com/github/mithunder/statements/ConstantValue.java
@@ -2,8 +2,8 @@ package com.github.mithunder.statements;
 
 public class ConstantValue extends Value {
 
-	public static final ConstantValue TRUE = null;
-	public static final ConstantValue FALSE = null;
+	public static final ConstantValue TRUE = getConstantValue(ValueType.BOOLEAN_TYPE, 1);
+	public static final ConstantValue FALSE = getConstantValue(ValueType.BOOLEAN_TYPE, 0);
 
 	protected final int value;
 
@@ -17,7 +17,35 @@ public class ConstantValue extends Value {
 		return true;
 	}
 
+	@Override
+	public int hashCode() {
+		return value;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		ConstantValue other;
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		other = (ConstantValue) obj;
+		return other.vtype == vtype && other.value == value;
+	}
+
 	public static ConstantValue getConstantValue(int vtype, int value) {
+		if(vtype == ValueType.BOOLEAN_TYPE){
+			/* no reason to make multiple versions of these */
+			if(value != 0) {
+				return TRUE;
+			}
+			return FALSE;
+		}
 		return new ConstantValue(vtype, value);
 	}
 }
