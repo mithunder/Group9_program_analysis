eda23dbf23c15d45f7fe4d51e55030c3649f6594
Niels Thykier <niels@thykier.net>
2010-10-15 10:05:23 +0200

    Added Annotations to StatementFactory.

---
+++ src/com/github/mithunder/statements/Annotation.java
@@ -0,0 +1,8 @@
+package com.github.mithunder.statements;
+
+public abstract class Annotation {
+
+	public static Annotation newInstance(String name, String value){
+		return null;
+	}
+}
--- src/com/github/mithunder/statements/StatementFactory.java
+++ src/com/github/mithunder/statements/StatementFactory.java
@@ -5,32 +5,32 @@ import java.util.List;
 public abstract class StatementFactory {
 
 	/*
-	* Simple three-address statement
-	* read assign
-	* write assign
-	* assign := v[1] op v[2] (binary op)
-	* assign := op v[1] (unary op)
-	* assign := v[1] (for type = assign)
-	* Number of arguments accepted for v is 1 or 2 depending on which type is used
-	*/
-	public abstract Statement createSimpleStatement(int type, CodeLocation codeLoc, Variable assign, Value ... v);
+	 * Simple three-address statement
+	 * read assign
+	 * write assign
+	 * assign := v[1] op v[2] (binary op)
+	 * assign := op v[1] (unary op)
+	 * assign := v[1] (for type = assign)
+	 * Number of arguments accepted for v is 1 or 2 depending on which type is used
+	 */
+	public abstract Statement createSimpleStatement(int type, CodeLocation codeLoc, List<Annotation> annotations, Variable assign, Value ... v);
 	//Meant for things like abort.
-	public abstract Statement createSimpleStatement(int type, CodeLocation codeLoc);
-	
-	/*
-	* Creates grouping statement
-	* type =  do/if/{}/AND/OR
-	* The result of a test is s[s.length - 1].getAssign() (should return temporary variable)
-	* 
-	*/
-	public abstract Statement createCompoundStatement(int type, CodeLocation codeLoc, Statement ... s);
-	public abstract Statement createCompoundStatement(int type, CodeLocation codeLoc, List<Statement> s);
+	public abstract Statement createSimpleStatement(int type, CodeLocation codeLoc, List<Annotation> annotations);
 
 	/*
-	* Method for creating a root node containing s as its children
-	*/
-	public abstract Statement createRootStatement(CodeLocation codeLoc, Statement ... s);
-	public abstract Statement createRootStatement(CodeLocation codeLoc, List<Statement> s);
+	 * Creates grouping statement
+	 * type =  do/if/{}/AND/OR
+	 * The result of a test is s[s.length - 1].getAssign() (should return temporary variable)
+	 *
+	 */
+	public abstract Statement createCompoundStatement(int type, CodeLocation codeLoc, List<Annotation> annotations, Statement ... s);
+	public abstract Statement createCompoundStatement(int type, CodeLocation codeLoc, List<Annotation> annotations, List<Statement> s);
+
+	/*
+	 * Method for creating a root node containing s as its children
+	 */
+	public abstract Statement createRootStatement(CodeLocation codeLoc, List<Annotation> annotations, Statement ... s);
+	public abstract Statement createRootStatement(CodeLocation codeLoc, List<Annotation> annotations, List<Statement> s);
 
 	public static StatementFactory newInstance() {
 		return null;
