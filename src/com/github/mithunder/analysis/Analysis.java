53b4425cf217e2e9a0c3f9a047c128599a8b3506
Niels Thykier <niels@thykier.net>
2010-10-16 16:46:30 +0200

    Added isForwardAnalysis() to Analysis.

--- src/com/github/mithunder/analysis/Analysis.java
+++ src/com/github/mithunder/analysis/Analysis.java
@@ -13,4 +13,10 @@ public abstract class Analysis {
 
 	public abstract Evaluation initEvaluation(Statement s);
 
+	/**
+	 * @return true if this is a forward analysis, false if it is a
+	 * backwards analysis.
+	 */
+	public abstract boolean isForwardAnalysis();
+
 }
