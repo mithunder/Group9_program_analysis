9f8b5fd2f2b9906bd6183d29269d119ace428317
Melvin <mithunder@gmail.com>
2010-11-12 12:58:48 +0100

    Made printrunner more flexible.

--- src/com/github/mithunder/runner/PrintRunner.java
+++ src/com/github/mithunder/runner/PrintRunner.java
@@ -20,16 +20,37 @@ import com.github.mithunder.worklist.RoundRobinWorklist;
 import com.github.mithunder.worklist.Worklist;
 
 public class PrintRunner {
+	
+	private enum Options {PRINT, RD};
 
 	public static void main(String[] args) throws Exception {
 
-		System.out.println("Please input the name of the program (enter for default):");
-
 		String fileName;
+		Options chosenOption = null;
 		BufferedReader reader = null;
 		try {
+			System.out.println("Please input the name of the program (enter for default):");
+			
 			reader = new BufferedReader(new InputStreamReader(System.in));
 			fileName = reader.readLine();
+			
+			System.out.println("Please choose an option:");
+			for (Options o : Options.values()) {
+				System.out.println(o.ordinal() + ": " + o.toString());
+			}
+			String option = reader.readLine();
+			boolean recognized = false;
+			for (Options o : Options.values()) {
+				if (o.ordinal() == Integer.parseInt(option)) {
+					recognized = true;
+					chosenOption = o;
+				}
+			}
+			if (!recognized) {
+				System.out.println("Didn't recognized option: " + option);
+				System.exit(0);
+			}
+			
 		}
 		finally {
 			if (reader != null) {
@@ -46,19 +67,31 @@ public class PrintRunner {
 		GuardCommandParser parser = new GuardCommandParser(tokens);
 
 		CompilationUnit unit;
-
+		
 		try {
 
 			program_return program_r = parser.program();
 			unit = program_r.compilationUnit;
-			Analysis ana = new ReachingDefinitionAnalysis();
-			Worklist wl = new RoundRobinWorklist();
-			StatementIterator staIte = new StatementIterator(new CodeWriter());
-			System.out.println("Starting analysis");
-			long st = System.currentTimeMillis();
-			EvaluatedStatement nroot = wl.run(ana, unit);
-			System.out.println("Finished analysis, time: " + (System.currentTimeMillis() - st) + "ms.");
-			staIte.tour(new CompilationUnit(unit.getUnitName(), nroot, unit.getVariableTable(), unit.getFinalStatements()));
+			
+			switch (chosenOption) {
+			case PRINT : {
+				StatementIterator staIte = new StatementIterator(new CodeWriter());
+				staIte.tour(unit);
+				break;
+			}
+			case RD : {
+				Analysis ana = new ReachingDefinitionAnalysis();
+				Worklist wl = new RoundRobinWorklist();
+				StatementIterator staIte = new StatementIterator(new CodeWriter());
+				System.out.println("Starting analysis");
+				long st = System.currentTimeMillis();
+				EvaluatedStatement nroot = wl.run(ana, unit);
+				System.out.println("Finished analysis, time: " + (System.currentTimeMillis() - st) + "ms.");
+				staIte.tour(new CompilationUnit(unit.getUnitName(), nroot, unit.getVariableTable(), unit.getFinalStatements()));
+				break;
+			}
+			}
+			
 		} catch (RecognitionException e)  {
 			e.printStackTrace();
 		}
