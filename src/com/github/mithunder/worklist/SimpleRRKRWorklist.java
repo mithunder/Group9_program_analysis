9b493eee8cb16fbb489a9d9ea2f1b7a1e9e1c2ff
Niels Thykier <niels@thykier.net>
2010-11-19 16:15:28 +0100

    Fixed the loopNest counter in SimpleRRKRWorklist.

--- src/com/github/mithunder/worklist/SimpleRRKRWorklist.java
+++ src/com/github/mithunder/worklist/SimpleRRKRWorklist.java
@@ -119,11 +119,13 @@ public class SimpleRRKRWorklist implements KillRepairAnalysisWorklist {
 			}
 			break;
 		case DO:
+			loopNest++;
 			if(isFwd){
 				changed = evaluateDoStatementForward(toEval, entry);
 			} else {
 				changed = evaluateDoStatementBackward(toEval, entry);
 			}
+			loopNest--;
 			break;
 		case SKIP:
 			/* Does not change the state of the program */
